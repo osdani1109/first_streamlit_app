18a36b201569eb77cb42ace4346491582c418cd4
Daniel Rodriguez <oscar.rodriguez@sofka.com.co>
2022-12-22 16:44:40 -0500

    new estructure

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
-st.title("my parents new healthy diner")
-st.text("breackfast menu")
-st.text("omega 3 & blueberry oatmeal") 
-st.text("kale. spinach & rocket smothie")
-st.text("har-boiled free-range eggs")
-
+st.title("breakfast favaourites")
+st.text("🥣omega 3 & blueberry oatmeal") 
+st.text("🥗kale. spinach & rocket smothie")
+st.text("🐔har-boiled free-range eggs")
+st.text("🥑🍞 avocado toast")
