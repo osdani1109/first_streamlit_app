440ac8cfe9afb164d067b325c7a7ccbf54884f66
Daniel Rodriguez <oscar.rodriguez@sofka.com.co>
2022-12-22 16:58:06 -0500

    feat: add dataframe for deploy

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title("breakfast favaourites")
 st.text("🥣omega 3 & blueberry oatmeal") 
@@ -7,3 +8,5 @@ st.text("🐔har-boiled free-range eggs")
 st.text("🥑🍞 avocado toast")
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
