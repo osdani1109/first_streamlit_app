0272faab4e91c514585ced9ae04cc7e3ac44d3cf
Daniel Rodriguez <oscar.rodriguez@sofka.com.co>
2022-12-22 16:37:02 -0500

    feat: add list for breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
-st.title("Hello World") 
+st.title("my parents new healthy diner")
+st.text("breackfast menu")
+st.text("omega 3 & blueberry oatmeal") 
+st.text("kale. spinach & rocket smothie")
+st.text("har-boiled free-range eggs")
 
