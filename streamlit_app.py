f08cff5f6e436fcf2e357d2497d3cf3ed695561e
Daniel Rodriguez <oscar.rodriguez@sofka.com.co>
2022-12-26 18:08:04 -0500

    fix error logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 st.dataframe(fruits_to_show)
 st.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 st.text(fruityvice_response.json())
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
