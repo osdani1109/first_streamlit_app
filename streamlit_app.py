5cbd809a9f99a074ba82d351aad53ef5cfabb3d3
Daniel Rodriguez <oscar.rodriguez@sofka.com.co>
2022-12-27 17:54:17 -0500

    feat change to choice furit

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 st.header("Fruityvice Fruit Advice!")
 try:
-    fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+    fruit_choice = st.text_input('What fruit would you like information about?')
     if not fruit_choice:
-        st.error("please select a fruit to get information")
+        st.error("please select a fruit to get information.")
     else:
         frutlyvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
         fruityvice_normalized = pd.json_normalize(frutlyvice_response.json())
