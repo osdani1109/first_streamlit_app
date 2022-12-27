ce898b14ae400fe4c37ecdce77e4026014c76f06
Daniel Rodriguez <oscar.rodriguez@sofka.com.co>
2022-12-27 17:27:58 -0500

    feat nueve text imput

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,6 @@ my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_row = my_cur.fetchall()
 st.header("The fruit load list contains:")
 st.dataframe(my_data_row)
+
+add_my_fruit = st.text_input('What fruit would you like to add?','jackfruit')
+st.write('The user entered ', add_my_fruit)
