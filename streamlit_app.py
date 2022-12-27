9232127347c2e41c1b33f2faa7f1c8e986b13773
Daniel Rodriguez <oscar.rodriguez@sofka.com.co>
2022-12-27 17:34:38 -0500

    feat nueve text imput

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,4 @@ st.dataframe(my_data_row)
 
 add_my_fruit = st.text_input('What fruit would you like to add?','jackfruit')
 st.write('Thanks for adding ', add_my_fruit)
+my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES ('add_my_fruit+')")
